ad20ee8aead124a8b2bb4b50dad21f0ca5be1edb
VictorRoe <victorroe819@gmail.com>
2024-02-20 18:43:32 -0500

    fix problem with svg element

--- src/components/MenuToggle.tsx
+++ src/components/MenuToggle.tsx
@@ -24,17 +24,17 @@ const Path: React.FC<PathProps> = ({ ...props }) => (
 export const MenuToggle: React.FC<MenuToggleProps> = ({ toggle }) => {
   return (
     <button onClick={toggle} className="btn-nav">
-      <svg width="70" height="19" viewBox="0 0 70 19">
+      <svg width="55" height="55" viewBox="0 0 70 12">
         <Path
           variants={{
             closed: { d: "M 2 3.5 L 65 3.5" },
-            open: { d: "M 3 16.5 L 65 2.5" },
+            open: { d: "M 3 29.5 L 50 2.5" },
           }}
         />
         <Path
           variants={{
             closed: { d: "M 2 13.346 L 55 13.346" },
-            open: { d: "M 3 2.5 L 65 16.5" },
+            open: { d: "M 3 2.5 L 50 29.5" },
           }}
         />
       </svg>
