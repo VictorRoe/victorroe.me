24ed54ff98d799f9509e2fc89c948bd423801b3c
VictorRoe <victorroe819@gmail.com>
2024-02-22 16:50:07 -0500

    Add responsive monitor 16/9

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,31 +8,34 @@ export const Footer = () => {
   return (
     <>
       <footer className="footer">
-        <a
-          href="https://twitter.com/victorroe_"
-          target="_blank"
-          className="line"
-        >
-          <span className="hover-underline-animation-footer">Twitter</span>
-        </a>
-        <a
-          className="line"
-          href="https://www.linkedin.com/in/victorrangelromero/"
-          target="_blank"
-        >
-          <span className="hover-underline-animation-footer">LinkedIn</span>
-        </a>
-        <a
-          className="line"
-          href="https://www.instagram.com/victorroe_/"
-          target="_blank"
-        >
-          <span className="hover-underline-animation-footer">Instagram</span>
-        </a>
-
-        <a className="pushRight" onClick={backToTop}>
-          back to top
-        </a>
+        <div className="children-footer1">
+          <a
+            href="https://twitter.com/victorroe_"
+            target="_blank"
+            className="line"
+          >
+            <span className="hover-underline-animation-footer">Twitter</span>
+          </a>
+          <a
+            className="line"
+            href="https://www.linkedin.com/in/victorrangelromero/"
+            target="_blank"
+          >
+            <span className="hover-underline-animation-footer">LinkedIn</span>
+          </a>
+          <a
+            className="line"
+            href="https://www.instagram.com/victorroe_/"
+            target="_blank"
+          >
+            <span className="hover-underline-animation-footer">Instagram</span>
+          </a>
+        </div>
+        <div className="children-footer2">
+          <a className="pushRight" onClick={backToTop}>
+            back to top
+          </a>
+        </div>
       </footer>
     </>
   );
