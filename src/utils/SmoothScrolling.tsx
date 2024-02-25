854189f786476e1a1e50c54c512e0343a20787d3
VictorRoe <victorroe819@gmail.com>
2024-02-24 23:17:09 -0500

    Change property lenisOption

--- src/utils/SmoothScrolling.tsx
+++ src/utils/SmoothScrolling.tsx
@@ -1,21 +1,18 @@
 import { ReactLenis } from "@studio-freight/react-lenis";
 
 function SmoothScrolling({ children }: { children: React.ReactNode }) {
+  const lenisOptions = {
+    lerp: 0.1,
+    duration: 0.5,
+    smoothTouch: true, //smooth scroll for touch devices
+    smooth: true,
+  };
 
-    const lenisOptions = {
-        lerp: 0.1,
-        duration: 1.5,
-        smoothTouch: true, //smooth scroll for touch devices
-        smooth: true,
-
-    };
-
-
-    return (
-        <ReactLenis root options={lenisOptions}>
-            {children}
-        </ReactLenis>
-    )
+  return (
+    <ReactLenis root options={lenisOptions}>
+      {children}
+    </ReactLenis>
+  );
 }
 
 export default SmoothScrolling;
